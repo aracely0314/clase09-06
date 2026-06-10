c55e44370f6de5ba93c934b7ac4d8eb6b265239d
aracely0314 <ara.gonzalezv@duocuc.cl>
2026-06-10 15:34:47 -0400

    Validacion de errores

--- ejercicio1listas.py
+++ ejercicio1listas.py
@@ -1,1 +1,15 @@
-notas=
+notas=[]
+
+for i in range(5):
+    while True:
+        try:
+            nota=int(input("Ingrese la nota del estudiante: "))
+            if nota < 1 or nota > 70:
+                print("Error: La nota debe estar entre 1 y 70.")
+                continue
+            else: 
+                notas.append(nota)
+        except ValueError:
+            print("Error: Por favor, ingrese un número entero para la nota entre 1 y 70.")   
+
+

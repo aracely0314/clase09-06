284a7b1c790334c777eb35bd6ccfa041dc111607
aracely0314 <ara.gonzalezv@duocuc.cl>
2026-06-10 16:06:58 -0400

    recorrido de lista y condicion para encontrar max y min

--- ejercicio5listas.py
+++ ejercicio5listas.py
@@ -8,3 +8,13 @@ for i in range(8):
         except ValueError:
             print("Error: Por favor, ingrese un número entero.")
     numeros.append(numero)
+numero_mayor=numeros[0]
+numero_menor=numeros[0]
+for i in range(len(numeros)):
+    if numeros[i]>numero_mayor:
+        numero_mayor=numeros[i]
+    if numeros[i]<numero_menor:
+        numero_menor=numeros[i]
+        
+print(f"Número mayor: {numero_mayor}")
+print(f"Número menor: {numero_menor}")

53335cbfb639aa034faaff3a85b9601ed57ace9a
aracely0314 <ara.gonzalezv@duocuc.cl>
2026-06-10 16:33:18 -0400

    Cambios, pruebas y arreglos finales

--- ejercicio6listas.py
+++ ejercicio6listas.py
@@ -10,31 +10,43 @@ while True:
     os.system("cls")
     print(menu)
     opcion=input("Seleccione una opción: ")
+    os.system("cls")
     if opcion=="1":
+        print("Agregar estudiante")
         nombre=input("Ingrese el nombre del estudiante: ").strip().title()
         estudiantes.append(nombre)
         print(f"Estudiante '{nombre}' agregado exitosamente.")
-        break
+        print("Volviendo al menú principal...")
     elif opcion=="2":
-        nombre=input("Ingrese el nombre del estudiante a buscar: ").strip().title()
-        if nombre in estudiantes:
-            print(f"Estudiante '{nombre}' encontrado en la lista.")
-        else:
-            print(f"Estudiante '{nombre}' no encontrado en la lista.")
+        print("Buscar estudiante")
+        buscar=input("Ingrese el nombre del estudiante a buscar: ").strip().title()
+        for i in range(len(estudiantes)):
+            if estudiantes[i] == buscar:
+                print(f"Estudiante '{buscar}' encontrado en la lista en la posición {i+1}.")
+                print("Volviendo al menú principal...")
+                break
+        if buscar not in estudiantes:
+            print(f"Estudiante '{buscar}' no encontrado en la lista.")
+            print("Volviendo al menú principal...")
     elif opcion=="3":
-        nombre=input("Ingrese el nombre del estudiante a eliminar: ").strip().title()
-        if nombre in estudiantes:
-            estudiantes.remove(nombre)
-            print(f"Estudiante '{nombre}' eliminado exitosamente.")
+        print("Eliminar estudiante")
+        eliminar=input("Ingrese el nombre del estudiante a eliminar: ").strip().title()
+        if eliminar in estudiantes:
+            estudiantes.remove(eliminar)
+            print(f"Estudiante '{eliminar}' eliminado exitosamente.")
+            print("Volviendo al menú principal...")
         else:
-            print(f"Estudiante '{nombre}' no encontrado en la lista.")
-            
+            print(f"Estudiante '{eliminar}' no encontrado en la lista.")
+            print("Volviendo al menú principal...")
     elif opcion=="4":
+        print("Mostrar estudiantes")
         if not estudiantes:
             print("No hay estudiantes en la lista.")
+            print("Volviendo al menú principal...")
         else:
             for i in range(len(estudiantes)):
                 print(f"Estudiante {i+1}: {estudiantes[i]}")
+            print("Volviendo al menú principal...")
     elif opcion=="5":
         print("Saliendo del programa...")
         break

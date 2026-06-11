b5554798226e13a6946a1655795466847a3dc7b4
aracely0314 <ara.gonzalezv@duocuc.cl>
2026-06-10 23:20:49 -0400

    tercera opcion agregada con condicion

--- ejerciciocombinado.py
+++ ejerciciocombinado.py
@@ -29,9 +29,9 @@ while True:
         continue
     elif opcion == "2":
         print("Agregar nota")
-        nombre_nota = input("Ingrese el nombre del estudiante al que desea agregar una nota: ").strip().title()
-        if nombre_nota not in estudiantes:
-            print(f"El estudiante {nombre_nota} no existe.")
+        nombre= input("Ingrese el nombre del estudiante al que desea agregar una nota: ").strip().title()
+        if nombre not in estudiantes:
+            print(f"El estudiante {nombre} no existe.")
             time.sleep(2)
             continue
         else:
@@ -40,18 +40,24 @@ while True:
                     nota=int(input("Ingrese la nota del estudiante: "))
                     if nota>=1 and nota<=70:
                         for i in range(len(estudiantes)):
-                            if nombre_nota in estudiantes:
-                                estudiantes[nombre_nota].append(nota)
+                            if nombre in estudiantes:
+                                estudiantes[nombre].append(nota)
                                 break
-                        print(f"Nota agregada con éxito al estudiante {nombre_nota}.")
+                        print(f"Nota agregada con éxito al estudiante {nombre}.")
                         break
                     else:
                         print("La nota debe estar entre 1 y 70.")
                 except ValueError:
                     print("Por favor, ingrese un número entero válido.")
-
     elif opcion == "3":
-        pass
+        print("Mostrar estudiantes")
+        if len(estudiantes) == 0:
+            print("No hay estudiantes en la lista.")
+            time.sleep(1)
+        else:
+            for c in range(len(estudiantes)):
+                print(f"Estudiante {c+1}: {nombre}")
+            time.sleep(3)
     elif opcion == "4":
         pass
     elif opcion == "5":
